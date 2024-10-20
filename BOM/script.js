b3cbb7308e05d85510bf3d9156bd0f4ccb25539a
Arti chaudhary <chaudhary.aarti1998@gmail.com>
2024-10-21 01:09:43 +0530

    fetch

---
+++ BOM/script.js
@@ -0,0 +1,18 @@
+let box = document.getElementById("product-list")
+let response = fetch("https://dummyjson.com/products")
+
+response.then((data)=>data.json()).then((value)=>{
+    
+    item = value.products
+    console.log(item)
+    
+    item.forEach((element, id) => {
+        let itemContainer = document.createElement('div')
+
+        itemContainer.innerHTML = `
+        <h1>${element.title}</h1>
+        <img src=${element.images} alt="Girl in a jacket" width="500" height="600">
+        `
+        box.appendChild(itemContainer)
+    });
+})
